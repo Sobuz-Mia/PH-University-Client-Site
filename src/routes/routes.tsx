6edc9924b1fb73464bcdbced608ff6ed42098729
sobuz <sobuz.info0298@gmail.com>
2024-07-07 03:32:09 +0600

    sidebar dynamically handle as role base

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,7 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import { adminRoutes } from "./admin.routes";
+import { adminPath } from "./admin.routes";
+import { routesGenerator } from "../utils/routesGenerator";
+import { facultyPath } from "./faculty.routes";
+import { studentPath } from "./student.routes";
 
 export const route = createBrowserRouter([
   {
@@ -12,7 +15,17 @@ export const route = createBrowserRouter([
   {
     path: "/admin",
     element: <App />,
-    children: adminRoutes,
+    children: routesGenerator(adminPath),
+  },
+  {
+    path: "/faculty",
+    element: <App />,
+    children: routesGenerator(facultyPath),
+  },
+  {
+    path: "/student",
+    element: <App />,
+    children: routesGenerator(studentPath),
   },
   {
     path: "/login",
