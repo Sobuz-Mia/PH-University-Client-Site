bcb441aa818339c7e4fcf26dc5d44d6b4310a768
Sobuz <sobuz.info0298@gmail.com>
2024-08-26 01:24:40 +0600

    Protected routes created

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,30 +1,38 @@
 import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
-import { setUser } from "../redux/features/auth/authSlice";
+import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyAccessToken } from "../utils/verifyAccessToken";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const { register, handleSubmit } = useForm({
     defaultValues: {
       id: "A-0001",
       password: "admin123",
     },
   });
-  const [login, { error }] = useLoginMutation();
-  const onsubmit = async (data) => {
-    const userInfo = {
-      id: data.id,
-      password: data.password,
-    };
-    const res = await login(userInfo).unwrap(); // here unwrap used for remove other layer
-    const user = verifyAccessToken(res.data.accessToken);
-    dispatch(setUser({ user: user, token: res?.data.accessToken }));
+  const [login] = useLoginMutation();
+  const onsubmit = async (data: FieldValues) => {
+    const toastId = toast.loading("Logging in", {});
+    try {
+      const userInfo = {
+        id: data.id,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap(); // here unwrap used for remove other layer
+      const user = verifyAccessToken(res.data.accessToken) as TUser;
+      dispatch(setUser({ user: user, token: res?.data.accessToken }));
+      navigate(`/${user?.role}/dashboard`);
+      toast.success("Logged in", { id: toastId, duration: 2000 });
+    } catch (err) {
+      toast.error("Something Want wrong", { id: toastId, duration: 2000 });
+    }
   };
-
-  console.log("This is a error =>", error);
   return (
     <form onSubmit={handleSubmit(onsubmit)}>
       <div>
@@ -35,7 +43,7 @@ const Login = () => {
         <label htmlFor="id">Password:</label>
         <input type="text" id="password" {...register("password")} />
       </div>
-      <Button htmlType="submit">Submit</Button>
+      <Button htmlType="submit">Log in</Button>
     </form>
   );
 };
